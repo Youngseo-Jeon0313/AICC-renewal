16859004bc9fdcf0401921070c1ab28e54ec53f7
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2025-05-16 22:51:50 +0900

    feat: mysql db를 docker 형태로 띄워 연결하도록 구현

--- appV1/config/database.py
+++ appV1/config/database.py
@@ -1,9 +1,10 @@
 import mysql.connector
+import os
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",  
-        password="root",  
-        database="aicc"  
+        host=os.getenv('MYSQL_HOST', 'mysql'),
+        user=os.getenv('MYSQL_USER', 'rootuser'),
+        password=os.getenv('MYSQL_PASSWORD', 'rootpw'),
+        database=os.getenv('MYSQL_DATABASE', 'aicc')
     )
